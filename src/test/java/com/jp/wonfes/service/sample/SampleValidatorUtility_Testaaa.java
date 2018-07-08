2a706d67de06ff9767cd4c7fafe8aed5ae7e76a2
Akihiro Iwata <iakihiro22@gmail.com>
2018-07-08 19:25:46 +0900

    Junitをお試しで作成

---
+++ src/test/java/com/jp/wonfes/service/sample/SampleValidatorUtility_Testaaa.java
@@ -0,0 +1,9 @@
+package com.jp.wonfes.service.sample;
+
+public class SampleValidatorUtility_Testaaa {
+
+	public SampleValidatorUtility_Testaaa() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
